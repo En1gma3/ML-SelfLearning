8922722fb21c749711fbd5a3f1238a154afbb210
Enigma <602503034@qq.com>
2020-07-09 16:45:54 +0800

    add some bag

---
+++ mglearn/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 15:48:43 2020
+
+@author: Administrator
+"""
+
+from . import plots
+
+from . import tools
+
+from .plots import cm3, cm2
+
+from .tools import discrete_scatter
+
+from .plot_helpers import ReBl
+
+
+
+__all__ = ['tools', 'plots', 'cm3', 'cm2', 'discrete_scatter', 'ReBl']
